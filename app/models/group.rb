e5166cbb134af98ead788a4f6996dd125583e816
ichala <alijandhd@gmail.com>
2022-09-03 08:57:38 +0100

    Add Validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
     belongs_to :user,
     has_many :trans
+
+    validates :name, presence: true, length: { maximum: 250 }
+    validates :icon, presence: true, length: { maximum: 250 }
 end
--- app/models/tran.rb
+++ app/models/tran.rb
@@ -1,4 +1,7 @@
 class Tran < ApplicationRecord
     belongs_to : :groups,
     belongs_to :user,
+
+    validates :amount, presence: true
+    validates :name, presence: true, length: { maximum: 250 }
 end
